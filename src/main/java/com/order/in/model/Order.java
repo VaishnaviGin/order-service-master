d947c98fb71ff2b1e7d88a2f3c9a2816eed2bce9
vginkala <vginkala@LIN-5CG136BD19.corp.capgemini.com>
2024-04-01 15:01:21 +0530

    first commit

---
+++ src/main/java/com/order/in/model/Order.java
@@ -0,0 +1,26 @@
+package com.order.in.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long orderId;
+    @Column
+    private String orderName;
+    @Column
+    private Integer quantity;
+    @Column
+    private Double amount;
+
+}
